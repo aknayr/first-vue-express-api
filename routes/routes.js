5cb1b8312ef1d767842798e73560a01a59ddcf30
Ajay Nayr <ajaynayr@gmail.com>
2022-05-12 20:54:56 -0500

    lab completed and deployed

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ let router = express.Router()
 // req represents the request the client has made
 // res presents the response the server sends back
 router.get('/', function(req,res, next){
-    res.json( { 'message': 'Hello!' } )
+    res.json( { 'message': 'Welcome to Server Programming!' } )
 })
 
 // module.export allows another file to require/access this file/router object
